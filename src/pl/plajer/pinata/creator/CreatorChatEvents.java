eb911a1bebd6439ca9e0fb00cd51d3759caa77de
Plajer <worthplay.pt@onet.pl>
2018-06-02 13:59:31 +0200

    Events for creator

---
+++ src/pl/plajer/pinata/creator/CreatorChatEvents.java
@@ -0,0 +1,29 @@
+package pl.plajer.pinata.creator;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Plajer
+ * <p>
+ * Created at 02.06.2018
+ */
+public class CreatorChatEvents implements Listener {
+
+    private Map<Player, ChatReaction> chatReactions = new HashMap<>();
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e) {
+        //todo
+    }
+
+    public enum ChatReaction {
+        SET_MOB_TYPE, SET_HEALTH, SET_CRATE_TIME, SET_DROP_VIEW_TIME, SET_BLINDNESS_DURATION, SET_FULL_BLINDNESS
+    }
+
+}
---
+++ src/pl/plajer/pinata/creator/CreatorEvents.java
@@ -0,0 +1,11 @@
+package pl.plajer.pinata.creator;
+
+/**
+ * @author Plajer
+ * <p>
+ * Created at 02.06.2018
+ */
+public class CreatorEvents {
+
+
+}
