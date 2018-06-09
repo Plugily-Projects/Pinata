22a5d1593dccfc3e70bfd2a19de3e8e00ebd1699
Plajer <worthplay.pt@onet.pl>
2018-06-09 19:05:18 +0200

    Basic chat reactions implemented

---
+++ src/pl/plajer/pinata/creator/ChatReaction.java
@@ -0,0 +1,23 @@
+package pl.plajer.pinata.creator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import pl.plajer.pinata.pinata.Pinata;
+
+/**
+ * @author Plajer
+ * <p>
+ * Created at 09.06.2018
+ */
+@Data
+@AllArgsConstructor
+public class ChatReaction {
+
+    private ReactionType reactionType;
+    private Pinata pinata;
+
+    public enum ReactionType {
+        SET_MOB_TYPE, SET_HEALTH, SET_CRATE_TIME, SET_DROP_VIEW_TIME, SET_BLINDNESS_DURATION, SET_FULL_BLINDNESS
+    }
+
+}
--- src/pl/plajer/pinata/creator/CreatorChatEvents.java
+++ src/pl/plajer/pinata/creator/CreatorChatEvents.java
@@ -1,9 +1,13 @@
 package pl.plajer.pinata.creator;
 
+import org.apache.commons.lang.math.NumberUtils;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import pl.plajer.pinata.ConfigurationManager;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,11 +23,47 @@ public class CreatorChatEvents implements Listener {
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent e) {
-        //todo
-    }
-
-    public enum ChatReaction {
-        SET_MOB_TYPE, SET_HEALTH, SET_CRATE_TIME, SET_DROP_VIEW_TIME, SET_BLINDNESS_DURATION, SET_FULL_BLINDNESS
+        if(chatReactions.containsKey(e.getPlayer())) {
+            ChatReaction reaction = chatReactions.get(e.getPlayer());
+            FileConfiguration config = ConfigurationManager.getConfig("pinata_storage");
+            switch(reaction.getReactionType()) {
+                case SET_MOB_TYPE:
+                    try {
+                        EntityType type = EntityType.valueOf(e.getMessage());
+                        reaction.getPinata().setEntityType(type);
+                        config.set("storage." + reaction.getPinata().getID() + ".mob-entity-type", type.name());
+                        e.getPlayer().sendMessage("Entity type of pinata " + reaction.getPinata().getID() + " has been set to " + type.name());
+                        chatReactions.remove(e.getPlayer());
+                    } catch(IllegalArgumentException ex) {
+                        e.getPlayer().sendMessage("That entity doesn't exist! Use following types:");
+                        StringBuilder mobs = new StringBuilder();
+                        for(EntityType en : EntityType.values()) {
+                            if(en == null) continue;
+                            mobs.append(en.getName()).append(", ");
+                        }
+                        e.getPlayer().sendMessage(mobs.toString());
+                        return;
+                    }
+                case SET_HEALTH:
+                    if(!NumberUtils.isNumber(e.getMessage())){
+                        e.getPlayer().sendMessage("Following message is not a number!");
+                        return;
+                    }
+                    reaction.getPinata().setHealth(Double.parseDouble(e.getMessage()));
+                    config.set("storage." + reaction.getPinata().getID() + ".health-amount", Double.parseDouble(e.getMessage()));
+                    e.getPlayer().sendMessage("Health of pinata " + reaction.getPinata().getID() + " has been set to " + e.getMessage());
+                    chatReactions.remove(e.getPlayer());
+                    break;
+                case SET_CRATE_TIME:
+                    break;
+                case SET_DROP_VIEW_TIME:
+                    break;
+                case SET_BLINDNESS_DURATION:
+                    break;
+                case SET_FULL_BLINDNESS:
+                    break;
+            }
+        }
     }
 
     public Map<Player, ChatReaction> getChatReactions() {
