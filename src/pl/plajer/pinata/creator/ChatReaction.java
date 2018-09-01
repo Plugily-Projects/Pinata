d6caaf6c11e1b2bee0432e4d895af4331b2a23d1
Plajer <worthplay.pt@onet.pl>
2018-09-01 12:31:25 +0200

    awesome lombok

--- src/pl/plajer/pinata/creator/ChatReaction.java
+++ src/pl/plajer/pinata/creator/ChatReaction.java
@@ -30,7 +30,6 @@ public class ChatReaction {
   private ReactionType reactionType;
   private Pinata pinata;
 
-  @java.beans.ConstructorProperties({"reactionType", "pinata"})
   public ChatReaction(ReactionType reactionType, Pinata pinata) {
     this.reactionType = reactionType;
     this.pinata = pinata;
@@ -50,38 +49,6 @@ public class ChatReaction {
 
   public void setPinata(Pinata pinata) {
     this.pinata = pinata;
-  }
-
-  public boolean equals(Object o) {
-    if (o == this) return true;
-    if (!(o instanceof ChatReaction)) return false;
-    final ChatReaction other = (ChatReaction) o;
-    if (!other.canEqual((Object) this)) return false;
-    final Object this$reactionType = this.getReactionType();
-    final Object other$reactionType = other.getReactionType();
-    if (this$reactionType == null ? other$reactionType != null : !this$reactionType.equals(other$reactionType)) return false;
-    final Object this$pinata = this.getPinata();
-    final Object other$pinata = other.getPinata();
-    if (this$pinata == null ? other$pinata != null : !this$pinata.equals(other$pinata)) return false;
-    return true;
-  }
-
-  public int hashCode() {
-    final int PRIME = 59;
-    int result = 1;
-    final Object $reactionType = this.getReactionType();
-    result = result * PRIME + ($reactionType == null ? 43 : $reactionType.hashCode());
-    final Object $pinata = this.getPinata();
-    result = result * PRIME + ($pinata == null ? 43 : $pinata.hashCode());
-    return result;
-  }
-
-  protected boolean canEqual(Object other) {
-    return other instanceof ChatReaction;
-  }
-
-  public String toString() {
-    return "ChatReaction(reactionType=" + this.getReactionType() + ", pinata=" + this.getPinata() + ")";
   }
 
   public enum ReactionType {
