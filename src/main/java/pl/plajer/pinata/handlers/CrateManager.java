1d8ad0380d42da0f4a0eeb56a415b05c360d81d6
Plajer <worthplay.pt@onet.pl>
2018-12-25 16:39:08 +0100

    Lower nesting for better readability

--- src/main/java/pl/plajer/pinata/handlers/CrateManager.java
+++ src/main/java/pl/plajer/pinata/handlers/CrateManager.java
@@ -72,50 +72,58 @@ public class CrateManager implements Listener {
 
   @EventHandler
   public void onCrateClick(PlayerInteractEvent e) {
-    if (e.getClickedBlock() != null && e.getClickedBlock().getType() != null && e.getClickedBlock().getType().equals(Material.CHEST) && e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
-      if (cratesLocations.containsKey(e.getClickedBlock().getLocation())) {
-        e.setCancelled(true);
-        if (!plugin.isPluginEnabled("Vault")) {
-          e.getPlayer().sendMessage(Utils.colorMessage("Pinata.Command.Vault-Not-Detected"));
-          return;
-        }
-        if (!e.getPlayer().hasPermission("pinata.player.crate")) {
-          e.getPlayer().sendMessage(Utils.colorMessage("Pinata.Crate-Creation.No-Permission"));
-          return;
-        }
-        if (!plugin.getConfig().getBoolean("disabled-worlds-exclusions.crates")) {
-          if (plugin.getDisabledWorlds().contains(e.getPlayer().getWorld().getName())) {
-            e.getPlayer().sendMessage(Utils.colorMessage("Pinata.Create.Disabled-World"));
-            return;
-          }
-        }
-        crateUsage.put(e.getPlayer(), e.getClickedBlock().getLocation());
-        Utils.createPinatasGUI("Menus.Crate-Menu.Inventory-Name", e.getPlayer());
+    if (e.getClickedBlock() == null || e.getClickedBlock().getType() == null) {
+      return;
+    }
+    if (!e.getClickedBlock().getType().equals(Material.CHEST) || e.getAction() != Action.RIGHT_CLICK_BLOCK) {
+      return;
+    }
+    if (!cratesLocations.containsKey(e.getClickedBlock().getLocation())) {
+      return;
+    }
+    e.setCancelled(true);
+    if (!plugin.isPluginEnabled("Vault")) {
+      e.getPlayer().sendMessage(Utils.colorMessage("Pinata.Command.Vault-Not-Detected"));
+      return;
+    }
+    if (!e.getPlayer().hasPermission("pinata.player.crate")) {
+      e.getPlayer().sendMessage(Utils.colorMessage("Pinata.Crate-Creation.No-Permission"));
+      return;
+    }
+    if (!plugin.getConfig().getBoolean("disabled-worlds-exclusions.crates")) {
+      if (plugin.getDisabledWorlds().contains(e.getPlayer().getWorld().getName())) {
+        e.getPlayer().sendMessage(Utils.colorMessage("Pinata.Create.Disabled-World"));
+        return;
       }
     }
+    crateUsage.put(e.getPlayer(), e.getClickedBlock().getLocation());
+    Utils.createPinatasGUI("Menus.Crate-Menu.Inventory-Name", e.getPlayer());
   }
 
   @EventHandler
   public void onCrateDestroy(BlockBreakEvent e) {
-    if (e.getBlock().getType().equals(Material.CHEST)) {
-      ConfigurationSection pinata = ConfigUtils.getConfig(plugin, "crates").getConfigurationSection("crates");
-      if (cratesLocations.containsKey(e.getBlock().getLocation())) {
-        for (String key : pinata.getKeys(false)) {
-          if (cratesLocations.get(e.getBlock().getLocation()).equals(key)) {
-            if (!e.getPlayer().hasPermission("pinata.admin.crate.destroy")) {
-              e.getPlayer().sendMessage(Utils.colorMessage("Pinata.Crate-Creation.No-Permission"));
-              e.setCancelled(true);
-            }
-            FileConfiguration config = ConfigUtils.getConfig(plugin, "crates");
-            config.set("crates." + key, null);
-            ConfigUtils.saveConfig(plugin, config, "crates");
-            cratesLocations.remove(e.getBlock().getLocation());
-            String message = Utils.colorMessage("Pinata.Crate-Creation.Destroyed");
-            e.getPlayer().sendMessage(message.replace("%name%", key));
-            return;
-          }
-        }
+    if (!e.getBlock().getType().equals(Material.CHEST)) {
+      return;
+    }
+    ConfigurationSection pinata = ConfigUtils.getConfig(plugin, "crates").getConfigurationSection("crates");
+    if (!cratesLocations.containsKey(e.getBlock().getLocation())) {
+      return;
+    }
+    for (String key : pinata.getKeys(false)) {
+      if (!cratesLocations.get(e.getBlock().getLocation()).equals(key)) {
+        continue;
       }
+      if (!e.getPlayer().hasPermission("pinata.admin.crate.destroy")) {
+        e.getPlayer().sendMessage(Utils.colorMessage("Pinata.Crate-Creation.No-Permission"));
+        e.setCancelled(true);
+      }
+      FileConfiguration config = ConfigUtils.getConfig(plugin, "crates");
+      config.set("crates." + key, null);
+      ConfigUtils.saveConfig(plugin, config, "crates");
+      cratesLocations.remove(e.getBlock().getLocation());
+      String message = Utils.colorMessage("Pinata.Crate-Creation.Destroyed");
+      e.getPlayer().sendMessage(message.replace("%name%", key));
+      return;
     }
   }
 
